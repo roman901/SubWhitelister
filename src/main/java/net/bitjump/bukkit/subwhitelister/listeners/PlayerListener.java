1a0f3b29508c50440a81e09fb864df38607e9633
Daniel Mudd <me@bitjump.net>
2014-08-27 14:10:32 +0100

    Add player listener.

---
+++ src/main/java/net/bitjump/bukkit/subwhitelister/listeners/PlayerListener.java
@@ -0,0 +1,29 @@
+package net.bitjump.bukkit.subwhitelister.listeners;
+
+import net.bitjump.bukkit.subwhitelister.SubWhitelister;
+import net.bitjump.bukkit.subwhitelister.util.WhitelistManager;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerLoginEvent.Result;
+
+public class PlayerListener implements Listener 
+{
+	@EventHandler
+	public void onPlayerLogin(PlayerLoginEvent e)
+	{
+		if(!SubWhitelister.config.getBoolean("enabled")) return;
+		
+		Player p = e.getPlayer();
+		
+		if(!p.hasPermission("subwhitelister.exempt"))
+		{
+			if(!WhitelistManager.getUsers().contains(p.getName()))
+			{
+				e.disallow(Result.KICK_WHITELIST, "Not on the whitelist.");
+			}
+		}
+	}
+}
